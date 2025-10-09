c49a2f8fa012498930e2255a6493f7d719cd121d
Tumisang Bogopa <bogopatumisang@gmail.com>
2025-10-09 07:52:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7142251965-d408b",
+  "appId": "1:799736016915:web:e966bef6394958bad1cfe6",
+  "apiKey": "AIzaSyDw1hSnqf4sEtSVZR582WjiX7-fkYV3-Rw",
+  "authDomain": "studio-7142251965-d408b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "799736016915"
+};
